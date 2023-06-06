5d9d17677b49f5d3f13aae5edccfcd4db43a1bab
tfmrodge <tim.rodgers@ubc.ca>
2023-06-06 17:53:30 -0400

    Mistake in run_preproc

--- run_preproc.sh
+++ run_preproc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=4:0:0
+#SBATCH --time=23:0:0
 #SBATCH --account=def-agiang01
 #SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=8G
-#SBATCH --job-name='run_vargrid'
-./inmap grid steady --config=cmd/inmap/configExampleGEMMACH.toml
+#SBATCH --job-name='run_preproc'
+./inmap preproc --config=cmd/inmap/configGEMMACH_Feb.toml
 #./inmap preproc --config=cmd/inmap/config_test_geos.toml
