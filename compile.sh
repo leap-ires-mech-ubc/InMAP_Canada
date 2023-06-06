8f806d8e9f3032d26a166ff2bb3c560302882f9d
tfmrodge <tim.rodgers@ubc.ca>
2023-06-06 17:36:36 -0400

    Upload version to be run for all months with preproc.go

--- compile.sh
+++ compile.sh
@@ -3,8 +3,7 @@
 #SBATCH --account=def-agiang01
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=1024
-#SBATCH --ntasks=1
-#SBATCH --job-name='test_preproc'
+#SBATCH --job-name='compile_inmap'
 #./inmap preproc --config=cmd/inmap/configExampleGEMMACH.toml
 module load go/1.18.3
-GO111MODULE=on go build ./cmd/inmap 
+GO111MODULE=on go build ./cmd/inmap
---
+++ run_vargrid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --time=4:0:0
+#SBATCH --account=def-agiang01
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=8G
+#SBATCH --job-name='run_vargrid'
+./inmap grid --config=cmd/inmap/configExampleGEMMACH.toml
+#./inmap preproc --config=cmd/inmap/config_test_geos.toml
